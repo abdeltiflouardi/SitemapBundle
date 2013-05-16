078c726b8c34da36eb06a553fb6fedd7ec3db7d7
OrangeTux <awoosterhoff@gmail.com>
2013-05-16 23:08:29 +0200

    Rewritten the way a url node will be created. Made config parameter 'entity' optional.

--- SitemapGenerator.php
+++ SitemapGenerator.php
@@ -13,13 +13,12 @@ class SitemapGenerator
 {
 
     /**
-     *
      * @var EntityManager $em
      */
     private $em;
 
     /**
-     * @var $router
+     * @var 
      */
     private $router;
 
@@ -28,6 +27,11 @@ class SitemapGenerator
      * @var array $configs 
      */
     private $configs;
+
+    /**
+     * @var DOMDocument $root
+     */
+    private $dom;
 
     /**
      *
@@ -45,104 +49,130 @@ class SitemapGenerator
     public function generate($returnString = false)
     {
         // Create dom object
-        $dom = new DOMDocument('1.0', 'UTF-8');
-        $dom->formatOutput = true;
-        $dom->substituteEntities = false;
+        $this->dom = new DOMDocument('1.0', 'UTF-8');
+        $this->dom->formatOutput = true;
+        $this->dom->substituteEntities = false;
         
             
         // Create <urlset> root tag
-        $urlset = $dom->createElement('urlset');
+        $urlset = $this->dom->createElement('urlset');
         
         // Add attribute of urlset
-        $xmlns = $dom->createAttribute('xmlns');
-        $urlsetText = $dom->createTextNode('http://www.sitemaps.org/schemas/sitemap/0.9'); 
+        $xmlns = $this->dom->createAttribute('xmlns');
+        $urlsetText = $this->dom->createTextNode('http://www.sitemaps.org/schemas/sitemap/0.9'); 
         $urlset->appendChild($xmlns);
         $xmlns->appendChild($urlsetText);
 
         // Iterate over all routes in the config.
         foreach($this->configs['routes'] as $route => $routeConfig) {
 
-            var_dump($this->configs['routes'][$route]);
-            // Fetch All entities
-            $entities = $this->em->getRepository($this->configs['routes'][$route]['entity'])->findAll();
-
-            /*
-             *  Generate <url> tags and bind them in urlset
-             *  <url>
-             *     <loc>link</loc>
-             *     <lastmod>date</lastmod>
-             *     <priority>date</priority>
-             *  </url>
-             */
-            $tags = array('loc', 'lastmod', 'priority');
-            foreach ($entities as $entity) {
-                $url = $dom->createElement('url');
-                foreach ($tags as $tag) {
-                    $text = $dom->createTextNode($this->getTagValue($this->configs['routes'][$route], $entity, $tag));
-                    $elem = $dom->createElement($tag);
-                    $elem->appendChild($text);
-
-                    $url->appendChild($elem);
+            // If entity in the config has been defined 
+            if (isset($this->configs['routes'][$route]['entity'])) {
+                
+                // Fetch All entities
+                $entities = $this->em->getRepository($this->configs['routes'][$route]['entity'])->findAll();
+                
+                // Creates an url node for each entity
+                foreach ($entities as $entity) {
+                    $url = $this->createUrlNode($this->configs['routes'][$route], $entity);  
+                    $urlset->appendChild($url);
                 }
-
+            } else {
+                $url = $this->createUrlNode($this->configs['routes'][$route]);
                 $urlset->appendChild($url);
             }
         }
 
-        $dom->appendChild($urlset);
+        $this->dom->appendChild($urlset);
 
         if ($returnString == false)
-            return $dom->save($this->configs['path']);
+            return $this->dom->save($this->configs['path']);
 
-        return $dom->saveXML();
+        return $this->dom->saveXML();
     }
 
     /**
-     * 
-     * @param Entity $entity
-     * @param string $tag
-     * @return string 
+     * Creates a url node which represents a singel url.
+     * @param array configs
      */
-    public function getTagValue($configs, $entity, $tag)
-        
+    public function createUrlNode($configs, $entity=null)
     {
-        if (!is_array($configs[$tag])) {
-            $method = 'get' . ucfirst($configs[$tag]);
-            if (method_exists($entity, $method)) {
-                $value = $entity->$method();
-
-                if ($value instanceof DateTime) {
-                    $value = $value->format('Y-m-d');
-                } else {
-                    $value = substr($value, 0, 100);
+        /*
+         *  Generate <url> tags and bind them in urlset
+         *  <url>
+         *     <loc>link</loc>
+         *     <lastmod>date</lastmod>
+         *     <priority>date</priority>
+         *  </url>
+         */
+        $tags = array('loc', 'lastmod', 'priority');
+        $url = $this->dom->createElement('url');
+        foreach ($tags as $tag) {
+            $text = $this->dom->createTextNode($this->{'create' . ucfirst($tag) . 'Node'}($configs, $entity));
+            $elem = $this->dom->createElement($tag);
+            $elem->appendChild($text);
+            $url->appendChild($elem);
+        }
+        return $url;
+    }
+    
+    /**
+     * Creates a loc node (<loc><loc>)
+     * @param array configs
+     */
+    public function createLocNode($configs, $entity=null)
+    {
+        $params = array();
+        foreach ($configs['loc']['params'] as $key => $param) {
+            if (is_array($param)) {
+                // params: { key: {value: <some value>, static: true}}
+                if (isset($param['static']) && $param['static']) { $params[$key]  = $param['value'];
+                // params: { key: {class: <some class>, method: <some method>, field: <some method>}}
+                } else {           
+                    $value        = $entity->{'get' . ucfirst($param['field'])}();
+                    $object       = new $param['class'];
+                    $params[$key] = $object->{$param['method']}($value);
                 }
             } else {
-                $value = $configs[$tag];
-            }
-
-            return $value;
-        } else {
-            extract($configs[$tag]);
-            
-            foreach ($params as $key => $param) {
-                if (is_array($param)) {
-                    // params: { key: {value: <some value>, static: true}}
-                    if(isset($param['static']) && $param['static']) {
-                        $params[$key]  = $param['value'];
-                    // params: { key: {class: <some class>, method: <some method>, field: <some method>}}
-                    } else {           
-                        $value        = $entity->{'get' . ucfirst($param['field'])}();
-                        $object       = new $param['class'];
-                        $params[$key] = $object->{$param['method']}($value);
-                    }
+                if(is_null($entity)) {
+                    var_dump($param);
+                    $params[$key]  = $param;
                 } else {
                     $value        = $entity->{'get' . ucfirst($param)}();
                     $params[$key] = $value;
                 }
             }
-            return $this->router->generate($route, $params, true);
         }
+        return $this->router->generate($configs['loc']['route'], $params, true);
     }
-}
 
+    /**
+     * Creates a lastmod node (<lastmod></lastmod>)
+     * @param array configs
+     */
+    public function createLastmodNode($configs, $entity=null)
+    {
+        if (!is_null($entity)) {
+            if (method_exists($entity, 'getLastmod()')) {
+                $value = $entity->getLastmod();
+
+                if ($value instanceof DateTime) {
+                    return $value->format('Y-m-d');
+                } else {
+                    return substr($value, 0, 100);
+                }
+            }
+        }
+        return $configs['lastmod'];
+            
+    }
+    
+    /**
+     * Creates a priority node (<node></node>)
+     * @param array configs
+     */
+    public function createPriorityNode($configs)
+    {
+        return $configs['priority'];
+    }
 ?>
